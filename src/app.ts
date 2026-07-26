73accc58a8224e7be333c5d9c769ba7b29520229
Rafiul Islam <irafiul210@gmail.com>
2026-07-26 23:47:02 +0600

    add express must-needed middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
+
+app.use(cors({ origin: config.app_url }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("This response if from the root route!");
